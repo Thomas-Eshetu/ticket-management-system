ade7470750c5d4aca9de16ae4e425d96069927ee
unknown <thomaseshetu1992@gmail.com>
2026-02-09 17:06:23 +0300

    Initial commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,4 +9,12 @@ class AdminController extends Controller
   public function dashboard(){
     return view("admin_pages.dashboard");
   }
+
+  public function addUserView(){
+    return view("admin_pages.addUser");
+  }
+
+  public function viewUserView(){
+    return view("admin_pages.viewUser");
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', function () {
 });
 
 Route::get('/admin-dashboard', [AdminController::class, 'dashboard']);
+Route::get('/add-user', [AdminController::class,'addUserView']);
+Route::get('/view-user', [AdminController::class,'viewUserView']);
