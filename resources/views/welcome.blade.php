b838b087439ddf0883a5ccead65394a8d184dedc
unknown <thomaseshetu1992@gmail.com>
2026-02-09 15:49:40 +0300

    first-commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/style.css">
+    <title>Ticketing System</title>
+</head>
+
+<body>
+
+    <div class="loginContainer">
+    <h3 class="title">Ticket Management System</h3>
+    <hr>
+
+    <form action="">
+        <input type="text" class="form-control mt-3" name="" id="" placeholder="Enter your username">
+        <input type="password" class="form-control mt-3" name="" placeholder="Enter your password">
+        <input type="submit" value="Login" class="btn btn-primary mt-3 w-100">
+    </form>
+
+    <a href="" class="forgotLink">Forgot Password?</a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
